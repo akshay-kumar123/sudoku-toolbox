aa8c16a7d5fbd519eec8a75e8efeda5c70e877ca
Axel <Axel@192.168.1.9>
2013-01-06 14:13:57 +1100

    Merged two args into an array in enum Difficulty.

--- src/sudoku/generator/Difficulty.java
+++ src/sudoku/generator/Difficulty.java
@@ -2,28 +2,28 @@ package sudoku.generator;
 
 public enum Difficulty {
 	
-	EXTREMELY_EASY(50, 60, 5),
-	EASY(36, 49, 4),
-	MEDIUM(32, 35, 3),
-	DIFFICULT(28, 31, 2),
-	EVIL(22, 27, 0);
+	EXTREMELY_EASY(new int[]{50, 60}, 5),
+	EASY(new int[]{36, 49}, 4),
+	MEDIUM(new int[]{32, 35}, 3),
+	DIFFICULT(new int[]{28, 31}, 2),
+	EVIL(new int[]{22, 27}, 0);
 
-	private int givensLowerBound, givensUpperBound, givensRowColLowerBound;
+	private int givensRowColLowerBound;
+	private int[] givensInterval;
 	
 	
-	Difficulty(int givensLowerBound, int givensUpperBound, int givensRowColLowerBound) {
-		this.givensLowerBound = givensLowerBound;
-		this.givensUpperBound = givensUpperBound;
+	Difficulty(int[] givensInterval, int givensRowColLowerBound) {
+		this.givensInterval = givensInterval;
 		this.givensRowColLowerBound = givensRowColLowerBound;
 	}
 
 	
 	public int getGivensLowerBound() {
-		return givensLowerBound;
+		return givensInterval[0];
 	}
 
 	public int getGivensUpperBound() {
-		return givensUpperBound;
+		return givensInterval[1];
 	}
 
 	public int getGivensRowColLowerBound() {
